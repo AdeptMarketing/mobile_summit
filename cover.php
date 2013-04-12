fc4ed13799d2f9610420bc8f30fdfe9eeb5b3d4a
justinhines57 <jhines@marketingadept.com>
2013-04-12 06:47:22 -0400

    added cover and fixed nav quirks

---
+++ cover.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<title>Mobile Marketing Summit | April 12 2013</title>
+	<style type="text/css" media="screen">
+		body {
+			background:#f58026;
+			text-align: center;
+			margin:0;
+			padding:0;
+		}
+	</style>
+</head>
+<body>
+	<a href="index.php"><img src="img/cover.png" alt="" style="margin-top:50px"></a>
+</body>
--- js/main.js
+++ js/main.js
@@ -8,11 +8,17 @@ $(document).ready(function(){
 		media: '(min-width: 600px)',
 		entry: function(){
 			$('a[href*=#]').slideto({speed  : 800});
-			$('section').waypoint(function() {
-				var linkNumber = $(this).parent().index() + 1;
-				$('#nav li').removeClass('current');
-				$('#nav li:nth-child('+linkNumber+')').addClass('current');
-			},{offset: '300px'});	
+			$('section')
+				.waypoint(function(direction) {
+					var linkNumber = $(this).parent().index() + 1;
+					$('#nav li').removeClass('current');
+					$('#nav li:nth-child('+linkNumber+')').addClass('current',direction === 'down');
+				},{offset: '300px'})
+				.waypoint(function(direction) {
+					var linkNumber = $(this).parent().index() + 1;
+					$('#nav li').removeClass('current');
+					$('#nav li:nth-child('+linkNumber+')').addClass('current',direction === 'up');
+				});
 		},
 		exit: function(){
 			
